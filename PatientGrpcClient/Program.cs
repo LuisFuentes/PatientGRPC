a8402c0b4121a3efe9a746b9ec28f3ea8007e3fd
Luis <luis90fuentes@gmail.com>
2019-11-15 13:49:46 -0800

    Added fetch all patients methods

--- PatientGrpcClient/Program.cs
+++ PatientGrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using PatientGrpcServer;
 using System;
 using System.Threading.Tasks;
@@ -13,20 +14,28 @@ namespace PatientGrpcClient
 
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
 
-            // var input = new HelloRequest { Name = "Luis" };
-            // var client = new Greeter.GreeterClient(channel);
-
+            // Fetch patient ID #1
             var input = new PatientLookupModel {  PatientId = 1};
             var client = new Patient.PatientClient(channel);
 
-
             var patient = await client.GetPatientInfoAsync(input);
-
             Console.WriteLine($"Patient { patient.FirstName } { patient.LastName }. Chart ID { patient.ChartId }");
+
+            // Fetch all patients on database
+            Console.WriteLine("All patients: ");
+            
+            using (var patients = client.GetAllPatientsInfo(new AllPatientsRequest()))
+            {
+                while (await patients.ResponseStream.MoveNext())
+                {
+                    var pt = patients.ResponseStream.Current;
+                    Console.WriteLine($"Patient { pt.FirstName } { pt.LastName }. Chart ID { pt.ChartId }");
+
+
+                }
+            }
             
             Console.ReadLine();
-
-
         }
     }
 }
--- PatientGrpcServer/Services/PatientService.cs
+++ PatientGrpcServer/Services/PatientService.cs
@@ -21,27 +21,55 @@ namespace PatientGrpcServer.Services
         }
 
         public override async Task<PatientModel> GetPatientInfo(
-            PatientLookupModel request, 
+            PatientLookupModel request,
             ServerCallContext context)
         {
 
             // Function is called when the GRPC server is requested a patient's information
             // with the request containing a patient ID to search for.
 
-            // TODO: Perform a database query
-            var patients = await _context.Patient.ToListAsync();
-
+            // Query for the patient by ID
             Models.Patient patient = await _context.Patient.FirstOrDefaultAsync(x => x.PatientId == request.PatientId);
 
             return await Task.FromResult(
-                new PatientModel { 
-                    FirstName = patient.FirstName, 
+                new PatientModel
+                {
+                    FirstName = patient.FirstName,
                     LastName = patient.LastName,
                     ChartId = patient.ChartId,
                     FacilityId = patient.FacilityId,
                     EpisodeId = patient.EpisodeId
                 }
             );
+        }
+
+        public override async Task GetAllPatientsInfo(
+            AllPatientsRequest request,
+            IServerStreamWriter<PatientModel> responseStream,
+            ServerCallContext context)
+        {
+            // Function fetches all patients on the database and streams the data back
+            // to the RPC client
+
+            // Fetch for all the patients
+            List<Models.Patient> patients = await _context.Patient.ToListAsync();
+
+            foreach (Models.Patient patient in patients)
+            {
+                // Write to patient data to the stream
+                await responseStream.WriteAsync(
+                    new PatientModel
+                    {
+                        FirstName = patient.FirstName,
+                        LastName = patient.LastName,
+                        ChartId = patient.ChartId,
+                        FacilityId = patient.FacilityId,
+                        EpisodeId = patient.EpisodeId
+                    }
+                );
+            }
+
+
         }
     }
 }
